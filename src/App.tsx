2a8fba53b56cf8d1bfbc816acf11feb9a01705db
Will <william.ingram@concentra.co.uk>
2021-08-03 15:24:26 +0100

    conveyor

--- src/App.tsx
+++ src/App.tsx
@@ -17,15 +17,31 @@ const Burger = ({ text }: { text: string } = { text: "Hamburger" }) => (
 
 function App() {
   const [text, setText] = useState("Hamburger")
+  const [burgers, setBurgers] = useState<string[]>([])
 
   return (
     <div className="App">
       <input
+        id="burgerName"
         className="hamburgerInput"
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
       <Burger text={text} />
+      <input
+        type="button"
+        onClick={() => {
+          setBurgers((current) => current.concat(text))
+          setText("Hamburger")
+        }}
+        value="New"
+      />
+      <div className="conveyor">
+        {burgers.map((burger, i) => (
+          <Burger key={i} text={burger} />
+        ))}
+      </div>
+      <input type="button" onClick={() => setBurgers([])} value="Clear" />
     </div>
   )
 }
